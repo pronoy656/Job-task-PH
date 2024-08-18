4ec16a57da3f95bd8bfa5a10dbe9e4ff3244c655
pronoy656 <pronoypaul56@gmail.com>
2024-08-18 17:57:08 +0600

    input field add

--- src/Component/Hooks/useProducts.jsx
+++ src/Component/Hooks/useProducts.jsx
@@ -1,12 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "./useAxiosPublic";
 
-const useProducts = () => {
+const useProducts = (search) => {
   const axiosPublic = useAxiosPublic();
   const { data: allProducts = [] } = useQuery({
     queryKey: ["allProducts"],
     queryFn: async () => {
-      const res = await axiosPublic.get("/products");
+      const res = await axiosPublic.get(`/products?search=${search}`);
       return res.data;
     },
   });
--- src/Component/Roo/AllProductsCard.jsx
+++ src/Component/Roo/AllProductsCard.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 
 const AllProductsCard = ({ product }) => {
-  const { phoneName, productImage } = product;
+  const {
+    phoneName,
+    productImage,
+    priceBDT,
+    category,
+    ratings,
+    productCreationDateTime,
+    description,
+  } = product;
   return (
-    <div>
-      <h1 className="text-xl font-medium">{phoneName}</h1>
-      <img className="w-36 h-32" src={productImage} alt="" />
+    <div className="border p-8">
+      <div className="flex justify-center">
+        <img className="w-40 h-36 border" src={productImage} alt="" />
+      </div>
+      <h1 className="text-xl font-medium">Name: {phoneName}</h1>
+      <h1>Price: {priceBDT} tk</h1>
+      <h1>Category: {category}</h1>
+      <h1>Ratings: {ratings}</h1>
+      <h1>Relase date: {productCreationDateTime}</h1>
+      <h1>Description: {description}</h1>
     </div>
   );
 };
